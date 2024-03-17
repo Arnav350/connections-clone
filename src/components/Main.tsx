abc8c026adab1553ea1ef3e5541cc76dd2aa0818
Arnav Patel <patelarnavm+github@gmail.com>
2024-03-17 18:21:19 -0400

    worked on main and square

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,43 +2,67 @@ import { useState } from "react";
 import { Square } from "./Square";
 import { FaCircle } from "react-icons/fa6";
 
+type TColor = "yellow" | "green" | "blue" | "purple";
+
 export interface ISquare {
   word: string;
-  color: "yellow" | "green" | "blue" | "purple";
+  color: TColor;
+  position: number;
 }
 
 export const Main = () => {
-  const arr: ISquare[] = [
-    { word: "Dirty", color: "yellow" },
-    { word: "Stand", color: "yellow" },
-    { word: "Up", color: "yellow" },
-    { word: "Tell", color: "yellow" },
-    { word: "Live", color: "yellow" },
-    { word: "Serious", color: "yellow" },
-    { word: "That", color: "yellow" },
-    { word: "Realize", color: "yellow" },
-    { word: "Champion", color: "yellow" },
-    { word: "Eyes", color: "yellow" },
-  ];
+  const [squares, setSquares] = useState<ISquare[]>([
+    { word: "Dirty", color: "blue", position: 0 },
+    { word: "Stand", color: "yellow", position: 1 },
+    { word: "Up", color: "purple", position: 2 },
+    { word: "Tell", color: "blue", position: 3 },
+    { word: "Live", color: "yellow", position: 4 },
+    { word: "Serious", color: "green", position: 5 },
+    { word: "That", color: "purple", position: 6 },
+    { word: "Realize", color: "purple", position: 7 },
+    { word: "Champion", color: "green", position: 8 },
+    { word: "Eyes", color: "yellow", position: 9 },
+    { word: "Book", color: "green", position: 10 },
+    { word: "Mouse", color: "blue", position: 11 },
+    { word: "Screen", color: "green", position: 12 },
+    { word: "Circle", color: "blue", position: 13 },
+    { word: "Bulb", color: "purple", position: 14 },
+    { word: "Sticky", color: "yellow", position: 15 },
+  ]);
   const [selectedList, setSelectedList] = useState<ISquare[]>([]);
   const [mistakes, setMistakes] = useState<number>(4);
 
-  function handleClick() {}
+  function handleShuffle() {
+    const positions: number[] = squares.map(({ position }) => position);
+    for (let i = positions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [positions[i], positions[j]] = [positions[j], positions[i]];
+    }
 
-  const [test, setTest] = useState<number[]>([1, 2, 3]);
+    setSquares((prevSquares) => prevSquares.map((elem, i) => ({ ...elem, position: positions[i] })));
+  }
+
+  function handleSubmit() {
+    const colorSet: Set<TColor> = new Set(selectedList.map((selected) => selected.color));
+
+    if (colorSet.size === 1) {
+      const swapList = squares.filter((square) => square.position > 3 && square.color === selectedList[0].color);
+      // setSquares((prevSquares) => prevSquares.map(())
+
+      // );
+    } else {
+      if (mistakes === 1) {
+      } else {
+        setMistakes(mistakes - 1);
+      }
+    }
+  }
 
   return (
     <div className="main__container">
       <h4 className="main__heading">Create four groups of four!</h4>
-      <div onClick={() => setTest((prevTest) => prevTest.slice().sort(() => Math.random() - 0.5))}>
-        {test.map((elem, i) => (
-          <p key={i} className="test" style={{ top: elem * 64, left: elem * 64 }}>
-            {elem}
-          </p>
-        ))}
-      </div>
       <div className="main__words">
-        {arr.map((square, i) => (
+        {squares.map((square, i) => (
           <Square
             key={i}
             square={{ ...square, word: square.word.toUpperCase() }}
@@ -50,14 +74,17 @@ export const Main = () => {
       <div className="main__mistakes">
         <p className="main__remaining">Mistakes remaining:</p>
         {[...Array(mistakes)].map((__, i) => (
-          <FaCircle key={i} color="#544c43" />
+          <FaCircle key={i} color="#5a594e" />
         ))}
       </div>
       <div className="main__buttons">
-        <button className="main__button main__shuffle">Shuffle</button>
+        <button className="main__button main__shuffle" onClick={handleShuffle}>
+          Shuffle
+        </button>
         <button
           className="main__button main__deselect"
-          style={selectedList.length ? {} : { borderColor: "#544c43", color: "#544c43", cursor: "default" }}
+          style={selectedList.length ? { borderColor: "#000", color: "#000" } : { cursor: "default" }}
+          disabled={selectedList.length === 0}
           onClick={() => setSelectedList([])}
         >
           Deselect All
@@ -69,6 +96,8 @@ export const Main = () => {
               ? { backgroundColor: "#000", borderColor: "#000", color: "#fff" }
               : { cursor: "default" }
           }
+          disabled={selectedList.length !== 4}
+          onClick={handleSubmit}
         >
           Submit
         </button>
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useMemo, useState } from "react";
+import { Dispatch, SetStateAction, useMemo } from "react";
 import { ISquare } from "./Main";
 
 interface IProps {
@@ -10,7 +10,7 @@ interface IProps {
 export const Square = ({ square, selectedList, setSelectedList }: IProps) => {
   const selected: boolean = useMemo(
     () => selectedList.some((selected) => selected.word === square.word),
-    [selectedList]
+    [selectedList, square.word]
   );
 
   function handleClick() {
@@ -22,10 +22,19 @@ export const Square = ({ square, selectedList, setSelectedList }: IProps) => {
   }
 
   return (
-    <button className="word__container" style={selected ? { backgroundColor: "#544c43" } : {}} onClick={handleClick}>
-      <h4 className="word__word" style={selected ? { color: "#fff" } : {}}>
-        {square.word}
-      </h4>
-    </button>
+    <div
+      className="word__container"
+      style={{ top: Math.floor(square.position / 4) * 88, left: (square.position % 4) * 128 }}
+    >
+      <button
+        className="word__box"
+        style={selected ? { backgroundColor: "#5a594e" } : selectedList.length === 4 ? { cursor: "default" } : {}}
+        onClick={handleClick}
+      >
+        <h4 className="word__word" style={selected ? { color: "#fff" } : {}}>
+          {square.word}
+        </h4>
+      </button>
+    </div>
   );
 };
